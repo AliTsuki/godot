831fc9124b561ed98f6ddec67df2089da9ddc2e8
Ali Tsuki <alitsukistudio@gmail.com>
2026-09-22 15:00:51 -0600

    Fix RenderSceneDataExtension errors from expose lighting buffers to renderer for compositor effects

--- servers/rendering/storage/render_data_extension.cpp
+++ servers/rendering/storage/render_data_extension.cpp
@@ -73,6 +73,13 @@ void RenderSceneDataExtension::_bind_methods() {
 	GDVIRTUAL_BIND(_get_view_projection, "view");
 
 	GDVIRTUAL_BIND(_get_uniform_buffer);
+	GDVIRTUAL_BIND(_get_directional_light_buffer);
+	GDVIRTUAL_BIND(_get_omni_light_buffer);
+	GDVIRTUAL_BIND(_get_spot_light_buffer);
+
+	GDVIRTUAL_BIND(_decal_atlas_get_texture);
+	GDVIRTUAL_BIND(_decal_atlas_get_texture_srgb);
+	GDVIRTUAL_BIND(_directional_shadow_get_texture);
 }
 
 Transform3D RenderSceneDataExtension::get_cam_transform() const {
@@ -108,6 +115,42 @@ Projection RenderSceneDataExtension::get_view_projection(uint32_t p_view) const 
 RID RenderSceneDataExtension::get_uniform_buffer() const {
 	RID ret;
 	GDVIRTUAL_CALL(_get_uniform_buffer, ret);
+	return ret;
+}
+
+RID RenderSceneDataExtension::get_directional_light_buffer() const {
+	RID ret;
+	GDVIRTUAL_CALL(_get_directional_light_buffer, ret);
+	return ret;
+}
+
+RID RenderSceneDataExtension::get_omni_light_buffer() const {
+	RID ret;
+	GDVIRTUAL_CALL(_get_omni_light_buffer, ret);
+	return ret;
+}
+
+RID RenderSceneDataExtension::get_spot_light_buffer() const {
+	RID ret;
+	GDVIRTUAL_CALL(_get_spot_light_buffer, ret);
+	return ret;
+}
+
+RID RenderSceneDataExtension::decal_atlas_get_texture() const {
+	RID ret;
+	GDVIRTUAL_CALL(_decal_atlas_get_texture, ret);
+	return ret;
+}
+
+RID RenderSceneDataExtension::decal_atlas_get_texture_srgb() const {
+	RID ret;
+	GDVIRTUAL_CALL(_decal_atlas_get_texture_srgb, ret);
+	return ret;
+}
+
+RID RenderSceneDataExtension::directional_shadow_get_texture() const {
+	RID ret;
+	GDVIRTUAL_CALL(_directional_shadow_get_texture, ret);
 	return ret;
 }
 
